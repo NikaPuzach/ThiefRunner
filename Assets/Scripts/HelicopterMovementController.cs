159f79a9957c7f158ceaf836a43b3a13f901338c
NorskaGames <norskagames@gmail.com>
2022-05-22 10:47:19 +0400

    Helicopter improved

--- Assets/Scripts/HelicopterMovementController.cs
+++ Assets/Scripts/HelicopterMovementController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
@@ -9,21 +10,9 @@ public class HelicopterMovementController : MonoBehaviour
 
     [SerializeField] Transform arm;
 
-    private void MovePropeller()
-    {
-        arm.DORotate(new Vector3(0, 360, 0), 0.1f).SetLoops(-1);
-    }
-
-    public void HelicopterFly()
-    {
-        MovePropeller();
-        transform.DORotate(Vector3.zero, 1);
-        transform.DOMove(new Vector3
-            (transform.position.x,
-            transform.position.y + 1,
-            transform.position.z + GrabbableItemManager.fuelAmount * 3),
-            GrabbableItemManager.fuelAmount);
-    }
+    [Header("Route points")]
+    [SerializeField] Transform takeOffPoint;
+    [SerializeField] NumberPlatform[] platforms;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -31,5 +20,56 @@ public class HelicopterMovementController : MonoBehaviour
         {
             UnitEvents.OnHelicopterEndFinish.Invoke(number);
         }
+    }
+
+    private NumberPlatform GetTragetPosition(int fuelAmount)
+    {
+        return platforms.FirstOrDefault(p => p.GoldMultiply == fuelAmount)
+            ?? platforms.First();
+    }
+
+    private void MovePropeller()
+    {
+        arm.DORotate(new Vector3(0, 180, 0), 0.1f, RotateMode.LocalAxisAdd).SetLoops(-1);
+    }
+
+    public void HelicopterFly()
+    {
+        MovePropeller();
+
+        StartCoroutine(FlightRoutine(GrabbableItemManager.fuelAmount));
+    }
+
+    IEnumerator FlightRoutine(int fuelAmount)
+    {
+        // Time for gain height
+        var takeOffDuration = 1f;
+
+        // Time for flying over 1 platform is inversly interpolated
+        var flightDurationMax = 1.3f;
+        var flightDurationMin = 0.9f;
+        var referencePlatformsCountMin = 3;
+        var referencePlatformsCountMax = 5;
+        //var flightEase = Ease.OutCirc;
+
+        // Time for rotate sideway to the camera
+        var finishDuration = 1f;
+
+        transform.DORotate(Vector3.zero, takeOffDuration);
+        transform.DOMove(takeOffPoint.position, takeOffDuration);
+
+        yield return new WaitForSeconds(takeOffDuration);
+
+        var targetPlatform = GetTragetPosition(fuelAmount);
+        var platformsCount = targetPlatform.GoldMultiply;
+        var lerpFactor = Mathf.InverseLerp(referencePlatformsCountMax, referencePlatformsCountMin, platformsCount);
+        var duration = Mathf.Lerp(flightDurationMin, flightDurationMax, lerpFactor) * platformsCount;
+
+        transform.DORotate(new Vector3(15, 0, 0), finishDuration);
+        transform.DOMove(targetPlatform.HelicopterTargetPont.position, duration);//.SetEase(flightEase);
+
+        yield return new WaitForSeconds(duration);
+
+        transform.DORotate(new Vector3(0, 90, 0), finishDuration);
     }
 }
--- Assets/Scripts/NumberPlatform.cs
+++ Assets/Scripts/NumberPlatform.cs
@@ -6,4 +6,7 @@ public class NumberPlatform : MonoBehaviour
 {
     [SerializeField] int goldMultiply;
     public int GoldMultiply => goldMultiply;
+
+    [SerializeField] Transform helicopterTargetPont;
+    public Transform HelicopterTargetPont => helicopterTargetPont;
 }
